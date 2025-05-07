b8bdf2012fc5104dcbfa8b55ad5f1ec6f249583a
Sharkie🍞 <theretrobiz@gmail.com>
2025-05-07 18:05:33 +0300

    🐛Fix task

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -14,18 +14,6 @@ EXPECTED_BAKE_TIME = 40
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(elapsed_time):
     return EXPECTED_BAKE_TIME - elapsed_time
-    """Calculate the bake time remaining.
-
-    :param elapsed_bake_time: int - baking time already elapsed.
-    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
-
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-
-    pass
-
 
 #TODO: Define the 'preparation_time_in_minutes()' function below.
 # You might also consider defining a 'PREPARATION_TIME' constant.
@@ -33,32 +21,10 @@ def bake_time_remaining(elapsed_time):
 # This will make it easier to do calculations.
 def preparation_time_in_minutes(number_of_layers):
     return number_of_layers * 2
-    """Calculate the bake time remaining.
-
-    :param elapsed_bake_time: int - baking time already elapsed.
-    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
-
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-    
-    pass
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
-    """Calculate the bake time remaining.
-
-    :param elapsed_bake_time: int - baking time already elapsed.
-    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
-
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-    
-    pass
 
 # TODO: Remember to go back and add docstrings to all your functions
 #  (you can copy and then alter the one from bake_time_remaining.)
